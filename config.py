747507e43294e748326243a0d6369115d7d0a4d5
gibso <gibson2304@mail.ru>
2021-10-21 18:06:40 +0300

    added some configuration

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 SHEET_TYPE = os.environ["SHEET_TYPE"]
 SHEET_PROJECT_ID = os.environ["SHEET_PROJECT_ID"]
 SHEET_PRIVATE_KEY_ID = os.environ["SHEET_PRIVATE_KEY_ID"]
-SHEET_PRIVATE_KEY = os.environ["SHEET_PRIVATE_KEY"]
+SHEET_PRIVATE_KEY = os.environ["SHEET_PRIVATE_KEY"].replace('\\n', '\n')
 SHEET_CLIENT_EMAIL = os.environ["SHEET_CLIENT_EMAIL"]
 SHEET_CLIENT_ID = os.environ["SHEET_CLIENT_ID"]
 SHEET_AUTH_URI = os.environ["SHEET_AUTH_URI"]
